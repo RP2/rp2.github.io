6acf429eda1985601ac7e15db0eba3e761a2d6f0
RP2 <RP2@users.noreply.github.com>
2022-01-11 19:49:23 -0800

    header & nav
    think i got it to a good spot, will have to see if it holds up on mobile. Try three.js another time.

--- README.md
+++ README.md
@@ -2,6 +2,6 @@ Personal portfolio site coded by hand.
 
 > Dependencies: 
  >> - jquery
- >> - three
+ >> - google fonts
 
 https://rp2.github.io/ or https://2briley.com/
---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+//header parallax scroll effect
+$(window).scroll(function () {
+    var ws = $(window).scrollTop();
+    $('#background').css({
+        'transform': `translate(0px, ${ws / 2}px)`
+    });
+    $('#middle').css({
+        'transform': `translate(0px, ${ws / 2.5}px)`
+    });
+    $('#title').css({
+        'transform': `translate(0px, ${ws / 4}px)`
+    });
+    $('#foreground').css({
+        'transform': `translate(0px, ${ws / 7}px)`
+    });
+});
+
+// click link with hash scrolls to div with matching id
+$('a[href*="#"]')
+	.not('[href="#"]')
+	.not('[href="#0"]')
+	.click(function (event) {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				event.preventDefault();
+				$('html, body').animate({
+					left: target.offset().left,
+					scrollTop: target.offset().top
+				}, 1000, function () {
+					var $target = $(target);
+					$target.focus();
+					if ($target.is(":focus")) {
+						return false;
+					} else {
+						$target.attr('tabindex', '-1');
+						$target.focus();
+					};
+				});
+			}
+		}
+	});
